7ad82e7f7892a54c41e8f6a6fc16e5682f0b92ed
i2god1314 <i2god1314@163.com>
2019-02-10 22:52:42 +0900

    Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?

--- src/leetcode/DivideTwoIntegersNo29.java
+++ src/leetcode/DivideTwoIntegersNo29.java
@@ -1,7 +1,24 @@
 package leetcode;
 
 class DivideTwoIntegersNo29 {
-    //    public int divide(int dividend, int divisor) {
+
+    /**
+     * Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+     *
+     * Return the quotient after dividing dividend by divisor.
+     *
+     * The integer division should truncate toward zero.
+     *
+     * Example 1:
+     * Input: dividend = 10, divisor = 3
+     * Output: 3
+     *
+     * Example 2:
+     * Input: dividend = 7, divisor = -3
+     * Output: -2
+     */
+
+//    public int divide(int dividend, int divisor) {
 //        boolean positiveFlag = false;
 //        if ((dividend < 0 && divisor < 0) || (0 < dividend && 0 < divisor)) {
 //            positiveFlag = true;
