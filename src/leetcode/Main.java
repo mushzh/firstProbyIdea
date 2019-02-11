18b51ea76ed1995cd899808ff7b8ae9ea301c9d5
i2god1314 <i2god1314@163.com>
2019-02-11 14:59:15 +0900

    No15 のテスト

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -1,10 +1,12 @@
 package leetcode;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("IntelliJ IDEA enjoy it!");
+        no15();
         no28to29();
-
     }
     private static void no28to29() {
 
@@ -16,5 +18,12 @@ public class Main {
         System.out.println(solu2.divide(7, -3));
         System.out.println(solu2.divide(-1, -1));
         System.out.println(solu2.divide(-2147483648, -1));
+    }
+    private static void no15() {
+
+        Three3SumNo15 solu = new Three3SumNo15();
+        int[] num = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = solu.threeSum(num);
+        System.out.println("aaa");
     }
 }
