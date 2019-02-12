2bb1517a58d5533b58fb6051b5cc23a365e62a95
i2god1314 <i2god1314@163.com>
2019-02-13 00:14:45 +0900

    No1 2sum

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -5,9 +5,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("IntelliJ IDEA enjoy it!");
+        no1();
         no15();
         no28to29();
     }
+
     private static void no28to29() {
 
         ImplementStrStrNo28 solu = new ImplementStrStrNo28();
@@ -19,11 +21,32 @@ public class Main {
         System.out.println(solu2.divide(-1, -1));
         System.out.println(solu2.divide(-2147483648, -1));
     }
+
     private static void no15() {
 
         Three3SumNo15 solu = new Three3SumNo15();
         int[] num = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> result = solu.threeSum(num);
         System.out.println("aaa");
+    }
+
+    private static void no1() {
+
+        TwoSumNo1 solu = new TwoSumNo1();
+        int[] num = {2, 7, 11, 15};
+        int[] num2 = {5, 75, 25};
+        int[] num3 = {3, 3};
+        int[] numRst = solu.twoSum(num, 9);
+        for (int i : numRst) {
+            System.out.println(i);
+        }
+        int[] numRst2 = solu.twoSum(num2, 100);
+        for (int j : numRst2) {
+            System.out.println(j);
+        }
+        int[] numRst3 = solu.twoSum(num3, 6);
+        for (int k : numRst3) {
+            System.out.println(k);
+        }
     }
 }
---
+++ src/leetcode/TwoSumNo1.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumNo1 {
+    /**
+     * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+     * <p>
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * <p>
+     * Example:
+     * <p>
+     * Given nums = [2, 7, 11, 15], target = 9,
+     * <p>
+     * Because nums[0] + nums[1] = 2 + 7 = 9,
+     * return [0, 1].
+     */
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] result = new int[2];
+//        int[] numsSort = nums.clone();
+//        Arrays.sort(numsSort);
+//        int j = nums.length - 1;
+//        for (int i = 0; i < nums.length - 1; ) {
+//            if (numsSort[i] + numsSort[j] == target) {
+//                result[0] = Arrays.binarySearch(nums, numsSort[i]);
+//                result[1] = Arrays.binarySearch(nums, numsSort[j]);
+//                break;
+//            } else if (numsSort[i] + numsSort[j] < target) {
+//                i++;
+//            } else {
+//                j--;
+//            }
+//        }
+//        return result;
+//    }
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;
+    }
+}
