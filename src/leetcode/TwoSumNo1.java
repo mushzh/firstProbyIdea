0bb673d64265d6c923993a18ff89eda148340e90
i2god1314 <i2god1314@163.com>
2019-02-24 23:20:14 +0900

    無視してよい

--- src/leetcode/TwoSumNo1.java
+++ src/leetcode/TwoSumNo1.java
@@ -34,16 +34,16 @@ public class TwoSumNo1 {
 //        }
 //        return result;
 //    }
-    public int[] twoSum(int[] numbers, int target) {
+    public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < numbers.length; i++) {
-            if (map.containsKey(target - numbers[i])) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
                 result[1] = i;
-                result[0] = map.get(target - numbers[i]);
+                result[0] = map.get(target - nums[i]);
                 return result;
             }
-            map.put(numbers[i], i);
+            map.put(nums[i], i);
         }
         return result;
     }
